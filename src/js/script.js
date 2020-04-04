62be20b42c915681e09faeb44d5b532f67303c3a
LuisBtz <luisbttf@gmail.com>
2020-04-03 19:45:41 -0600

    v3

--- src/js/script.js
+++ src/js/script.js
@@ -204,6 +204,11 @@ $('span.spe').on('click', function(){
 });
 
 
+$('.x').on('click', function(){
+	$('.spects').removeClass('spects-ri');
+});
+
+
 
 
 
@@ -211,6 +216,7 @@ $('span.spe').on('click', function(){
 $('#carrusel-items').slick({
 	autoplay: true,
 	infinite: true,
+	dots: true,
 	pauseOnHover: false,
 	slidesToShow: 1,
 	arrows: false,
@@ -228,6 +234,29 @@ $('#carrusel-items').slick({
   });
 
 
+
+  $(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    inDuration: 1500,
+    outDuration: 1000,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    loading: true,
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
 
 
 
